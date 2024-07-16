6e9f88576977091445d707e525b35c5053fee3b7
Eric Moore <ericmoore0709@gmail.com>
2024-07-16 10:20:08 -0700

    added schema and configured db

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 /** Common config for bookstore. */
+require('dotenv').config();
 
-
-let DB_URI = `postgresql://`;
+let DB_URI = process.env.DATABASE_URL;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+  DB_URI = `${DB_URI}-test`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+  DB_URI = DB_URI;
 }
 
 
